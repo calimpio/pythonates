7248f9540fb40f79adbe8215cbc581dc5498d6ec
Camilo Barbosa <cab331@hotmail.com>
2020-08-16 23:13:02 -0500

    subtract operator

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from point import Point
 
 a = Point(20,20)
 b = Point(36,25)
-c = Point(a.x-b.x, a.y-b.y)
+c = a - b
 
 print 'x: ', c.x, ', y: ', c.y
 
--- point.py
+++ point.py
@@ -2,3 +2,5 @@ class Point:
     def __init__(self,x, y):
         self.x = x
         self.y = y
+    def __sub__(self, other):
+        return Point(self.x-other.x, self.y-other.y)
