5f99cc9b4150eea3c5622479df8c2875f112667b
Camilo Barbosa <cab331@hotmail.com>
2020-08-17 00:59:41 -0500

    list del element

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from entity import Entity, Point
 
 a = Entity("object 1")
 
-entities = [a, Entity("object 2"), Entity("object 3")]
-for ob in entities:
+objects = [a, Entity("object 2"), Entity("object 3")]
+for ob in objects:
+    print ob
+print 'selected: ', objects[1]
+
+box = objects[1]
+
+#remove
+del objects[1]
+
+for ob in objects:
     print ob
 
-
-
+print 'in box: ', box
